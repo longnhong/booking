58a32648771d34294f042d46866c1cc33ccea9bd
longnhong <longnhong23@gmail.com>
2018-06-07 13:45:07 +0700

    LongTM update cetm when cus update

--- api/mrw_booking/ticket/mrw_handler_update_cus.go
+++ api/mrw_booking/ticket/mrw_handler_update_cus.go
@@ -3,7 +3,6 @@ package ticket
 import (
 	ctrl "cetm_booking/ctrl_to_cetm"
 	"cetm_booking/o/auth"
-	"cetm_booking/o/auth/user"
 	"cetm_booking/o/ticket_onl"
 	"cetm_booking/x/math"
 	"cetm_booking/x/rest"
@@ -12,22 +11,15 @@ import (
 )
 
 func (s *ticketServer) handlerUpdateTicketCus(ctx *gin.Context) {
+	var usr, _ = auth.GetUserFromToken(ctx.Request)
 	var body = ticket_onl.TicketUpdate{}
 	rest.AssertNil(ctx.BindJSON(&body))
 	var extra, _ = json.Marshal(body)
 	var ticket = s.actionChange(body.BTicketID, "", ticket_onl.BookingCustomerUpdate, extra)
-	var usr *user.User
-	if body.TypeTicket == ticket_onl.TypeNow && ticket.TypeTicket == ticket_onl.TypeSchedule {
-		usr, _ = auth.GetUserFromToken(ctx.Request)
-		var timeNow = math.GetTimeNowVietNam()
-		if math.CompareDayTime(timeNow, body.TimeGoBank) == 0 {
-			ctrl.UpdateCounterTkCetm(usr, ticket)
-		}
-	} else {
-		auth.GetFromToken(ctx.Request)
-	}
-	if usr != nil {
-		ctrl.CreateNumCetm(usr, ticket)
+
+	var timeNow = math.GetTimeNowVietNam()
+	if math.CompareDayTime(timeNow, body.TimeGoBank) == 0 {
+		ctrl.UpdateCounterTkCetm(usr, ticket)
 	}
 	s.SendData(ctx, ticket)
 }
